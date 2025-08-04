88b5c4d076aa69b3e3594a2949ccf91958278dee
bhavishyalimendo <bhavishya.negi@limendo.com>
2025-08-04 12:03:46 +0530

    increase container

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   console.log("Home component rendering with airportData:", airportData);
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="container-xl mx-auto p-4">
       {/* <h1 className="text-2xl font-bold mb-6">Airport Security System</h1> */}
       {/* <p className="text-gray-600 mb-6">Interactive diagram of airport security components and their relationships.</p>       */}
       <EntityDiagram
